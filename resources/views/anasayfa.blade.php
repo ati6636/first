3d763fc27684e592d7476ba2a8815233a1724bf0
sedatişlek <sdt6@hotmail.com>
2022-07-09 00:21:38 +0300

    Route Kullanarak Anasayfa - Kurumsal - Referanslar - İletişim Sayfalarına Bağlanmayı Öğrendik

---
+++ resources/views/anasayfa.blade.php
@@ -0,0 +1,5 @@
+Burası anasayfa <br />
+
+<a href="/kurumsal">Kurumsal</a><br>
+<a href="/referanslar">Referanslar</a><br>
+<a href="/iletisim">İletişim</a>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+  return view('anasayfa');
+});
+Route::get ('/kurumsal', function(){
+  return view('kurumsal');
+});
+Route::get ('/referanslar', function(){
+  return view('referanslar');
+});
+Route::get ('/iletisim', function(){
+  return view('iletisim');
 });
