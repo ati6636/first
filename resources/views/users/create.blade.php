5a2e416ad1c3eced5ee7f2628488eaf2abe1a1da
sedatişlek <sdt6@hotmail.com>
2022-07-10 20:50:47 +0300

    RESOURCE Kullanımı ve controller oluşturma yönlendirme

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,1 @@
+Burası create sayfası
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,41 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-//REDİRECT(YÖNLENDİRME)
-Route::redirect('/','anasayfa');
-
-Route::get('/anasayfa', function(){
-  return view('anasayfa');
-});
-Route::get ('/kurumsal', function(){
-  return view('kurumsal');
-});
-Route::get ('/referanslar', function(){
-  return view('referanslar');
-});
-Route::get ('/iletisim', function(){
-  return view('iletisim');
-});
-
-//PREFİX KULLANIMI (GRUPLAMA)
-Route::prefix('sayfalar')->group( function(){
-
-  Route::get ('/kurumsal', function(){
-    return view('kurumsal');
-  });
-  Route::get ('/referanslar', function(){
-    return view('referanslar');
-  });
-  Route::get ('/iletisim', function(){
-    return view('iletisim');
-  });
-  //FALLBACK Kullanımı
-  Route::fallback(function(){
-    return view('hata');
-  });
-});
-//FALLBACK Kullanımı
-Route::fallback(function(){
-  return view('hata');
-});
+//RESOURCE Kullanımıı 
+Route::resource('users', UsersController::class);
