7979de7fdf4ce4b3fce27ec763a843b4827ebdb5
sedatişlek <sdt6@hotmail.com>
2022-07-11 20:53:13 +0300

    switch-case-break-default ifaleri kullanımı

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
             geçerli değer {{$i}}<br><br>
           @endfor -->
 
-          <!--foreach ifadesi-->
+          <!--foreach ifadesi
           @php
             $users=['cem', 'taner','ali']
           @endphp
@@ -47,7 +47,25 @@
             @foreach ($users as $user)
               <li>{{$user}}</li><br>
             @endforeach
-          @endif
+          @endif -->
+
+          <!--switch ifadesi-->
+          @php
+            $data='1';
+          @endphp
+          @switch($data)
+            @case(1)
+              numara 1'dir.
+            @break
+            @case(2)
+              numara 2'dir.
+            @break
+            @case(3)
+              numara 3'dür.
+            @break
+            @default
+            default değer
+          @endswitch
         </div>
     </body>
 </html>
